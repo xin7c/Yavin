60bd8faa102302997931f2a8c9ed28365f1dff99
xuchu@cmcm.com <xuchu@cmcm.com>
2020-10-14 16:32:13 +0800

    #update airtest & pocoui libs

--- utils/kafkaConsumer.py
+++ utils/kafkaConsumer.py
@@ -5,13 +5,42 @@
 @file:kafkaConsumer.py
 @time:2020/10/12
 """
+import time
 
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, TopicPartition
+from json import loads
+
+topic = 'xx5'
+
+
+def consumerAction():
+    consumer = KafkaConsumer(
+        topic,  # topic
+        group_id="xuchu",
+        bootstrap_servers=['10.61.153.83:9092',
+                           '10.61.153.83:9093',
+                           '10.61.153.83:9094'],  # bootstrap server
+        # api_version=(0, 11, 3),
+        auto_offset_reset='earliest',
+        # enable_auto_commit=True,
+        # value_deserializer=lambda x: loads(x.decode('utf-8'))
+    )
+    consumer.partitions_for_topic(topic)
+    print(consumer.topics())
+    # print(consumer.position(TopicPartition(topic=u'xx5', partition=0)))
+    consumer.subscribe(topics=['xx5'])
+    print('Receiving message...')
+    # 手动拉取消息
+    # while True:
+    #     msg = consumer.poll(timeout_ms=5)  # 从kafka获取消息
+    #     print(msg)
+    #     time.sleep(1)
+    for message in consumer:
+        recv = "%s:%d:%d: key=%s value=%s" % (
+            message.topic, message.partition, message.offset, message.key, message.value)
+        print(recv)
+        print('Message: ' + str(message.value))
+
 
 if __name__ == '__main__':
-    consumer = KafkaConsumer('test', bootstrap_servers=['10.61.153.83:9092'])
-
-    print(consumer)
-    for msg in consumer:
-        recv = "%s:%d:%d: key=%s value=%s" % (msg.topic, msg.partition, msg.offset, msg.key, msg.value)
-        print(recv)
+    consumerAction()
--- utils/kafkaProducer.py
+++ utils/kafkaProducer.py
@@ -5,18 +5,28 @@
 @file:kafkaProducer.py
 @time:2020/10/12
 """
+import time
+from json import dumps
+
 from kafka import KafkaProducer
 
-
+topic = "xx5"
 def conn_kafka():
-    producer = KafkaProducer(bootstrap_servers=['10.61.153.83:9092'])  # 连接kafka
+    producer = KafkaProducer(bootstrap_servers=['10.61.153.83:9092',
+                                                '10.61.153.83:9093',
+                                                '10.61.153.83:9094'])  # 连接kafka
+    print(producer.bootstrap_connected())
+    msg = f"{time.time()}".encode('utf-8')  # 发送内容,必须是bytes类型
+    future = producer.send(topic=topic, value=msg, key=b"xuchu")  # 发送的topic为test
+    result = future.get(timeout=60)
+    print(result)
+    print(future.is_done)
 
-    msg = "Hello World".encode('utf-8')  # 发送内容,必须是bytes类型
-    producer.send('xc_topic', msg)  # 发送的topic为test
-    # result = future.get(timeout=10)
-    # print(result)
     producer.close()
 
 
 if __name__ == '__main__':
-    conn_kafka()
+    for i in range(1000):
+        conn_kafka()
+        time.sleep(2)
+
